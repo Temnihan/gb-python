1116d6a09b16bbda271688b717dd7932486fa412
Рулан <temnihan@mail.ru>
2022-10-05 12:52:31 +0300

    shufle v2

--- 2Day/shufle.py
+++ 2Day/shufle.py
@@ -1,7 +1,10 @@
-from random import shuffle
+# реалезуйте алгоритм перемешивание
+from operator import le
+from random import random, shuffle, sample
 import os
 os.system('cls')
 array1 = [i for i in range(10)]
 print(array1)
-shuffle(array1)
+array3 = sample(array1,len(array1))
+# array2 = shuffle(array1)
 print(array1)
